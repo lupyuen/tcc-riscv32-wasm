34de0c76803c93241ba0c0f5b2d65d52b3aaf8d4
Lee Lup Yuen <luppy@appkaki.com>
2024-01-28 12:45:38 +0000

    Switching to comptime formatting

--- zig/tcc-wasm.zig
+++ zig/tcc-wasm.zig
@@ -215,7 +215,7 @@ fn format_string1(
     }
 
     ////
-    if (std.mem.eql(u8, spec, "%s")) {
+    if (std.mem.eql(u8, spec, "%d")) {
         debug("********", .{});
     }
     ////
@@ -273,7 +273,7 @@ fn format_string2(
     }
 
     ////
-    if (std.mem.eql(u8, spec, "%s")) {
+    if (std.mem.eql(u8, spec, "%d")) {
         debug("********", .{});
     }
     ////
@@ -323,34 +323,13 @@ export fn vsnprintf(str: [*:0]u8, size: size_t, format: [*:0]const u8, ...) c_in
         // Do Nothing
     } else if (format_string1(&ap2, str, size, format_slice, "%s", "{s}", [*:0]const u8)) {
         // Do Nothing
+    } else if (format_string1(&ap2, str, size, format_slice, "%d", "{}", c_int)) {
+        // Do Nothing
     } else if (format_cnt == 0) {
         // If no Format Specifiers: Return the Format, like `warning: `
         debug("vsnprintf: size={}, format={s}, format_cnt={}", .{ size, format, format_cnt });
         _ = memcpy(str, format, strlen(format));
         str[strlen(format)] = 0;
-    } else if (format_cnt == 1 and std.mem.containsAtLeast(u8, format_slice, 1, "%d")) {
-        // Format a Single `%d`, like `#define __TINYC__ %d`
-        var ap = @cVaStart();
-        defer @cVaEnd(&ap);
-        const i = @cVaArg(&ap, c_int);
-        debug("vsnprintf: size={}, format={s}, i={}", .{ size, format, i });
-
-        // Format the string
-        const format2 = "{}"; // Equivalent to C: `%d`
-        var buf: [100]u8 = undefined; // Limit to 100 chars
-        const buf_slice = std.fmt.bufPrint(&buf, format2, .{i}) catch {
-            wasmlog.Console.log("*** vsnprintf error: buf too small", .{});
-            @panic("*** vsnprintf error: buf too small");
-        };
-
-        // Replace the Format Specifier
-        var buf2 = std.mem.zeroes([100]u8); // Limit to 100 chars
-        _ = std.mem.replace(u8, format_slice, "%d", buf_slice, &buf2);
-
-        // Return the string
-        const len = std.mem.indexOfScalar(u8, &buf2, 0).?;
-        _ = memcpy(str, &buf2, @intCast(len));
-        str[len] = 0;
     } else {
         debug("TODO: vsnprintf: size={}, format={s}, format_cnt={}", .{ size, format, format_cnt });
         _ = memcpy(str, format, strlen(format));
