b20869cd23c5c23a6958525393364576505464ef
Lee Lup Yuen <luppy@appkaki.com>
2024-02-03 01:46:07 +0000

    Add Build Script

---
+++ zig/build.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+#  Build and Test TCC for WebAssembly
+
+## TODO: Set PATH
+export PATH=/workspaces/bookworm/xpack-riscv-none-elf-gcc-13.2.0-2/bin:$PATH
+export PATH=/workspaces/bookworm/zig-linux-x86_64-0.12.0-dev.2341+92211135f:$PATH
+
+set -e  #  Exit when any command fails
+set -x  #  Echo commands
+
+## Compile TCC from C to WebAssembly
+function build_wasm {
+  # zig translate-c \
+  #   -target wasm32-freestanding \
+  #   -rdynamic \
+  #   -lc \
+  #   tcc.c \
+  #   -DTCC_TARGET_RISCV64 \
+  #   -DCONFIG_TCC_CROSSPREFIX="\"riscv64-\""  \
+  #   -DCONFIG_TCC_CRTPREFIX="\"/usr/riscv64-linux-gnu/lib\"" \
+  #   -DCONFIG_TCC_LIBPATHS="\"{B}:/usr/riscv64-linux-gnu/lib\"" \
+  #   -DCONFIG_TCC_SYSINCLUDEPATHS="\"{B}/include:/usr/riscv64-linux-gnu/include\""   \
+  #   -DTCC_GITHASH="\"main:b3d10a35\"" \
+  #   -I. \
+  #   >/tmp/tcc.zig
+
+  zig cc \
+    -c \
+    -target wasm32-freestanding \
+    -dynamic \
+    -rdynamic \
+    -lc \
+    tcc.c \
+    -DTCC_TARGET_RISCV64 \
+    -DCONFIG_TCC_CROSSPREFIX="\"riscv64-\""  \
+    -DCONFIG_TCC_CRTPREFIX="\"/usr/riscv64-linux-gnu/lib\"" \
+    -DCONFIG_TCC_LIBPATHS="\"{B}:/usr/riscv64-linux-gnu/lib\"" \
+    -DCONFIG_TCC_SYSINCLUDEPATHS="\"{B}/include:/usr/riscv64-linux-gnu/include\""   \
+    -DTCC_GITHASH="\"main:b3d10a35\"" \
+    -Wall \
+    -O2 \
+    -Wdeclaration-after-statement \
+    -fno-strict-aliasing \
+    -Wno-pointer-sign \
+    -Wno-sign-compare \
+    -Wno-unused-result \
+    -Wno-format-truncation \
+    -Wno-stringop-truncation \
+    -I.
+
+  ## Dump our Compiled WebAssembly
+  wasm-objdump -h tcc.o
+  wasm-objdump -x tcc.o >/tmp/tcc.txt
+
+  ## Compile our Zig App `tcc-wasm.zig` for WebAssembly
+  ## and link with TCC compiled for WebAssembly
+  zig build-exe \
+    -target wasm32-freestanding \
+    -rdynamic \
+    -lc \
+    -fno-entry \
+    -freference-trace \
+    --verbose-cimport \
+    --export=compile_program \
+    zig/tcc-wasm.zig \
+    tcc.o
+
+  ## Dump our Linked WebAssembly
+  wasm-objdump -h tcc-wasm.wasm
+  wasm-objdump -x tcc-wasm.wasm >/tmp/tcc-wasm.txt
+
+  ## Copy the Linked TCC WebAssembly to the Web Server
+  cp tcc-wasm.wasm docs/
+
+  ## Run our Linked WebAssembly
+  node zig/test.js
+  node zig/test.js | grep "TODO"
+}
+
+## Test TCC WebAssembly with NuttX QEMU
+function test_wasm {
+  node zig/test-nuttx.js
+
+  pushd ../nuttx
+  cp /tmp/a.out ../apps/bin/
+  riscv-none-elf-objdump \
+    --syms --source --reloc --demangle --line-numbers --wide \
+    --debugging \
+    ../apps/bin/a.out
+  read -p "Press Enter..."
+  # script /tmp/a.log \
+    qemu-system-riscv64 \
+      -semihosting \
+      -M virt,aclint=on \
+      -cpu rv64 \
+      -smp 8 \
+      -bios none \
+      -kernel nuttx \
+      -nographic
+  riscv-none-elf-objdump \
+    --syms --source --reloc --demangle --line-numbers --wide \
+    --debugging \
+    ../apps/bin/a.out
+  popd
+}
+
+## Go to TCC Folder
+pushd ..
+
+## Compile TCC from C to WebAssembly
+build_wasm
+
+## Test TCC WebAssembly with NuttX QEMU
+test_wasm
+
+## Return to Zig Folder
+popd
