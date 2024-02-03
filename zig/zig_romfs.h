1a479784c05c9922028aab160c56a615ada118ce
Lee Lup Yuen <luppy@appkaki.com>
2024-02-03 11:31:29 +0000

    Build OK

--- zig/zig_romfs.h
+++ zig/zig_romfs.h
@@ -5,38 +5,32 @@
 #include <stdio.h>
 #include <stdint.h>
 
-struct mtd_geometry_s
-{
+struct mtd_geometry_s {
   uint32_t blocksize;     /* Size of one read/write block. */
-  uint32_t erasesize;     /* Size of one erase blocks -- must be a multiple
-                           * of blocksize. */
+  uint32_t erasesize;     /* Size of one erase blocks -- must be a multiple of blocksize. */
   uint32_t neraseblocks;  /* Number of erase blocks */
-
-  /* NULL-terminated string representing the device model */
-
-  char     model[NAME_MAX + 1];
+  char     model[NAME_MAX + 1]; /* NULL-terminated string representing the device model */
 };
 
 struct mm_map_entry_s {
-  uint8_t length;
-  uint8_t offset;
-  uint8_t rm_xipbase;
-  uint8_t *vaddr;
+  void *vaddr;
+  size_t length;
+  off_t offset;
 };
 
-int nxrmutex_init(FAR rmutex_t *rmutex);
-int nxrmutex_destroy(FAR rmutex_t *rmutex);
-int nxrmutex_lock(FAR rmutex_t *rmutex);
-int nxrmutex_unlock(FAR rmutex_t *rmutex);
+int nxrmutex_init(rmutex_t *rmutex);
+int nxrmutex_destroy(rmutex_t *rmutex);
+int nxrmutex_lock(rmutex_t *rmutex);
+int nxrmutex_unlock(rmutex_t *rmutex);
 
-FAR void *kmm_zalloc(size_t size);
+void *kmm_zalloc(size_t size);
 
-static ssize_t mtd_bread(FAR struct mtd_dev_s *dev,
-                         off_t block,
-                         size_t nsectors,
-                         FAR uint8_t *buf);
-static int mtd_ioctl(FAR struct mtd_dev_s *dev,
-                     int cmd,
-                     unsigned long arg);
+ssize_t mtd_bread(struct mtd_dev_s *dev,
+  off_t block,
+  size_t nsectors,
+  uint8_t *buf);
+int mtd_ioctl(struct mtd_dev_s *dev,
+  int cmd,
+  unsigned long arg);
 
 #endif  // __ZIG_ROMFS_H
