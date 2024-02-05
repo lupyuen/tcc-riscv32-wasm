8b0f2c61296dfba15da7b09e3394d308ea6d4277
Lee Lup Yuen <luppy@appkaki.com>
2024-02-05 09:35:30 +0000

    Hello World OK yay!

--- zig/romfs/stdio.h
+++ zig/romfs/stdio.h
@@ -1,1 +1,66 @@
 int puts(const char *s);
+
+// Caution: This may change
+#define SYS_write 61
+
+typedef int size_t;
+typedef int ssize_t;
+typedef int uintptr_t;
+
+// Make a System Call with 3 parameters...
+inline ssize_t write(int parm1, const void * parm2, size_t parm3) {
+  return (ssize_t) sys_call3(
+    (unsigned int) SYS_write,  // System Call Number
+    (uintptr_t) parm1,         // File Descriptor (1 = Standard Output)
+    (uintptr_t) parm2,         // Buffer to be written
+    (uintptr_t) parm3          // Number of bytes to write
+  );
+}
+
+// Make a System Call with 3 parameters
+inline uintptr_t sys_call3(
+  unsigned int nbr,  // System Call Number
+  uintptr_t parm1,   // First Parameter
+  uintptr_t parm2,   // Second Parameter
+  uintptr_t parm3    // Third Parameter
+) {
+  // Pass the Function Number and Parameters in
+  // Registers A0 to A3
+  register long r3 asm("a3") = (long)(parm3);  // Will move to A3
+  asm volatile ("slli a3, a0, 32");  // Shift 32-bit Left then Right
+  asm volatile ("srli a3, a3, 32");  // To clear the top 32 bits
+
+  register long r2 asm("a2") = (long)(parm2);  // Will move to A2
+  asm volatile ("slli a2, a0, 32");  // Shift 32-bit Left then Right
+  asm volatile ("srli a2, a2, 32");  // To clear the top 32 bits
+
+  register long r1 asm("a1") = (long)(parm1);  // Will move to A1
+  asm volatile ("slli a1, a0, 32");  // Shift 32-bit Left then Right
+  asm volatile ("srli a1, a1, 32");  // To clear the top 32 bits
+
+  register long r0 asm("a0") = (long)(nbr);  // Will remain in A0
+
+  // `ecall` will jump from RISC-V User Mode
+  // to RISC-V Supervisor Mode
+  // to execute the System Call.
+  // Input + Output Registers: A0 to A3
+  // Clobbers the Memory
+  asm volatile
+  (
+    // ECALL for System Call to NuttX Kernel
+    "ecall \n"
+    
+    // NuttX needs NOP after ECALL
+    ".word 0x0001 \n"
+
+    // Input+Output Registers: None
+    // Input-Only Registers: A0 to A3
+    // Clobbers the Memory
+    :
+    : "r"(r0), "r"(r1), "r"(r2), "r"(r3)
+    : "memory"
+  );
+
+  // Return the result from Register A0
+  return r0;
+} 
